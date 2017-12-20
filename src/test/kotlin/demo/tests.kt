d05e75be58f349e839d28e96c9d1779d535ac456
APO <ringo1625@gmail.com>
2017-12-20 15:44:31 +0900

    Update tests.kt

--- src/test/kotlin/demo/tests.kt
+++ src/test/kotlin/demo/tests.kt
@@ -5,6 +5,6 @@ import org.junit.Test
 
 class TestSource {
     @Test fun f() {
-        assertEquals("Hello, world!", getGreeting())
+        assertEquals("Hello, Okinawa!", getGreeting())
     }
 }
